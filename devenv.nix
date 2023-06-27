f7a4a6da5e9bdb97fa94c6e5b901654658bc6d95
Yanda Huang <realyanda@hey.com>
2023-06-26 22:46:45 -0700

    Init with first 2 blogs

---
+++ devenv.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+    hugo
+  ];
+
+  # https://devenv.sh/scripts/
+  scripts.server.exec = ''
+    hugo server
+  '';
+
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
